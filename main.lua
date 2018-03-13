f8cbf3cc7a14626e2eb28beed6ecb3e265f040ef
Milan Gruner <milangruner@gmail.com>
2018-03-13 07:15:01 +0100

    Remove extreme time-dependant scaling from Background and make jump animation slower

--- main.lua
+++ main.lua
@@ -69,7 +69,7 @@ function love.load()
 
     Player.sprite = love.graphics.newImage("sprites/ananas.png")
     local grid = anim8.newGrid(Player.width, Player.height, Player.sprite:getWidth(), Player.sprite:getHeight())
-    Player.animation = anim8.newAnimation(grid(1, 1, 2, 1, 3, 1, 2, 1), 0.2, "pauseAtEnd") -- TODO more frames
+    Player.animation = anim8.newAnimation(grid(1, 1, 2, 1, 3, 1, 2, 1), 0.3, "pauseAtEnd") -- TODO more frames
 
     bumpWorld = bump.newWorld()
     bumpWorld:add(Player, Player.x, Player.y, Player.width, Player.height)
